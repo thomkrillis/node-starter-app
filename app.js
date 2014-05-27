93391be069c3d3965ee6408f67c7ed0297291694
Bobby Yankou <thisisjackolantern@gmail.com>
2014-05-27 16:31:10 -0400

    NSA-6: extend requirejs format to app and User files

--- app.js
+++ app.js
@@ -1,33 +1,61 @@
-var express = require('express'),
-    morgan = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    expressSession = require('express-session'),
-    app = express(),
-    secrets = require('./config/secrets.js'),
-    port = secrets.port,
-    http = require('http'),
-    mongoose = require('mongoose'),
-    path = require('path'),
-    passport = require('passport'),
-    flash = require('connect-flash');
+var requirejs = require('requirejs');
 
-mongoose.connect(secrets.db);
+requirejs.config({nodeRequire: require});
+requirejs([
+  'express',
+  'morgan',
+  'cookie-parser',
+  'body-parser',
+  'express-session',
+  './config/secrets',
+  'http',
+  'mongoose',
+  'path',
+  'passport',
+  'connect-flash'
+],function(
+  express,
+  morgan,
+  cookieParser,
+  bodyParser,
+  expressSession,
+  secrets,
+  http,
+  mongoose,
+  path,
+  passport,
+  flash
+) {
+  module.exports = express;
+  module.exports = morgan;
+  module.exports = cookieParser;
+  module.exports = bodyParser;
+  module.exports = expressSession;
+  module.exports = secrets;
+  module.exports = http;
+  module.exports = mongoose;
+  module.exports = path;
+  module.exports = passport;
+  module.exports = flash;
+  app = express();
+  port = secrets.port;
+  mongoose.connect(secrets.db);
 
-require('./lib/passport')(passport);
+  require('./lib/passport')(passport);
 
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser());
-app.use(expressSession({ secret: secrets.sessionSecret }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.set('view engine', 'ejs');
-app.use(flash());
+  app.use(morgan('dev'));
+  app.use(cookieParser());
+  app.use(bodyParser());
+  app.use(expressSession({ secret: secrets.sessionSecret }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.set('view engine', 'ejs');
+  app.use(flash());
 
-require('./app/routes/api')(app, passport);
-require('./app/routes/client')(app, passport);
-//require('./app/routes/oAuth')(app, passport);
+  require('./app/routes/api')(app, passport);
+  require('./app/routes/client')(app, passport);
+  //require('./app/routes/oAuth')(app, passport);
 
-app.listen(port);
-console.log('listening on port ' + port);
+  app.listen(port);
+  console.log('listening on port ' + port);
+});
--- app/models/User.js
+++ app/models/User.js
@@ -1,40 +1,47 @@
-var mongoose = require('mongoose'),
-    bcrypt = require('bcrypt-nodejs');
+//var mongoose = require('mongoose'),
+//    bcrypt = require('bcrypt-nodejs');
 
-var userSchema = mongoose.Schema({
-  local: {
-    email: { type: String, unique: true },
-    password: String
-  },
-  
-  facebook: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  },
+require([
+  'mongoose',
+  'bcrypt'
+],function(mongoose, bcrypt) {
 
-  twitter: {
-    id: String,
-    token: String,
-    displayName: String,
-    username: String
-  },
+  var userSchema = mongoose.Schema({
+    local: {
+      email: { type: String, unique: true },
+      password: String
+    },
+    
+    facebook: {
+      id: String,
+      token: String,
+      email: String,
+      name: String
+    },
 
-  google: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  }
+    twitter: {
+      id: String,
+      token: String,
+      displayName: String,
+      username: String
+    },
+
+    google: {
+      id: String,
+      token: String,
+      email: String,
+      name: String
+    }
+  });
+
+  userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  };
+
+  userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+  };
+
+  module.exports = mongoose.model('User', userSchema);
+
 });
-
-userSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
-};
-
-module.exports = mongoose.model('User', userSchema);
